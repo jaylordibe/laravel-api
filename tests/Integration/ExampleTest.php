6da83ce5ef2738ceb3c587eb070db894fa26da50
jaylordibe <jaylordibe@gmail.com>
2023-06-17 12:46:53 +0800

    Updated deployment scripts

---
+++ tests/Integration/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    /**
+     * A basic test example.
+     */
+    public function testUnit(): void
+    {
+        $this->assertTrue(true);
+    }
+
+}
