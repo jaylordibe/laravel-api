445c40514ca56b5fcfdc4ab679e83dd64ad5e0c1
jaylordibe <jaylordibe@gmail.com>
2023-07-20 18:27:08 +0800

    Updated pipelines

--- test-pipeline.sh
+++ test-pipeline.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -a
 
+echo -e "\033[0m \033[1;35m Creating .env file \033[0m"
+cp .env.example .env
+source .env
+
 echo -e "\033[0m \033[1;35m Starting services \033[0m"
 docker compose up -d laravel-api laravel-db-test
+
+echo -e "\033[0m \033[1;35m Waiting for the containers to initialize \033[0m"
+while ! docker exec laravel-db-test mysql -uroot -p$TEST_DB_ROOT_PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
+    sleep 1
+done
 
 COMMANDS="
     chmod -R 777 storage
