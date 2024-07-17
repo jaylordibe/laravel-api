fffc8fca347ce482b9ef525f7e50336d178534e6
jaylordibe <jaylordibe@gmail.com>
2024-07-18 03:31:41 +0800

    Upgrade to laravel 11

--- test-pipeline.sh
+++ test-pipeline.sh
@@ -3,6 +3,7 @@ set -e
 
 ./vendor/bin/sail up -d
 ./vendor/bin/sail composer install
-./vendor/bin/sail artisan migrate
-./vendor/bin/sail exec -t laravel.test ./vendor/bin/paratest
+./vendor/bin/sail artisan migrate:fresh --seed --env=testing
+echo -e "\n" | ./vendor/bin/sail artisan passport:client --personal --env=testing
+./vendor/bin/sail exec -T laravel.test ./vendor/bin/paratest
 ./vendor/bin/sail down
