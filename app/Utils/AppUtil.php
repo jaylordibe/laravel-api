ab634c31939ce2a86ff993e46af7563b9b4588e1
jaylordibe <jaylordibe@gmail.com>
2024-08-05 20:28:47 +0800

    Upgrade to laravel 11

--- app/Utils/AppUtil.php
+++ app/Utils/AppUtil.php
@@ -8,7 +8,7 @@ class AppUtil
 {
 
     /**
-     * Check if email is valid.
+     * Check if an email is valid.
      *
      * @param string $email
      *
@@ -20,7 +20,21 @@ class AppUtil
     }
 
     /**
-     * Generate unique token.
+     * Check if a Philippine phone number is valid.
+     *
+     * @param string $phoneNumber
+     *
+     * @return bool
+     */
+    public static function isValidPhilippinePhoneNumber(string $phoneNumber): bool
+    {
+        $pattern = '/^(?:\+?63|0)\d{10}$/';
+
+        return (bool) preg_match($pattern, $phoneNumber);
+    }
+
+    /**
+     * Generate a unique token.
      *
      * @param string $prefix
      *
@@ -29,6 +43,18 @@ class AppUtil
     public static function generateUniqueToken(string $prefix = ''): string
     {
         return $prefix . now()->format('YmdHis') . Str::upper(Str::random());
+    }
+
+    /**
+     * Generate an OTP.
+     *
+     * @param int $length
+     *
+     * @return string
+     */
+    public static function generateOtp(int $length = 6): string
+    {
+        return str_pad(rand(0, pow(10, $length) - 1), $length, '0', STR_PAD_LEFT);
     }
 
 }
