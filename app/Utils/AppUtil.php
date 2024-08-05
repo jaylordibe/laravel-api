af7327978d8852f950fedb5d64f8255ca36b5e8b
jaylordibe <jaylordibe@gmail.com>
2024-08-05 20:09:29 +0800

    Upgrade to laravel 11

--- app/Utils/AppUtil.php
+++ app/Utils/AppUtil.php
@@ -2,16 +2,33 @@
 
 namespace App\Utils;
 
+use Illuminate\Support\Str;
+
 class AppUtil
 {
 
     /**
      * Check if email is valid.
+     *
      * @param string $email
+     *
      * @return bool
      */
     public static function isValidEmail(string $email): bool
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
+
+    /**
+     * Generate unique token.
+     *
+     * @param string $prefix
+     *
+     * @return string
+     */
+    public static function generateUniqueToken(string $prefix = ''): string
+    {
+        return $prefix . now()->format('YmdHis') . Str::upper(Str::random());
+    }
+
 }
