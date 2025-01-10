03ca59ad67915fd5718f9b6d2b0ccb4d07219080
jaylordibe <jaylordibe@gmail.com>
2025-01-10 11:02:10 +0800

    Created RoleRepository

---
+++ app/Repositories/RoleRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role;
+
+class RoleRepository
+{
+
+    /**
+     * Find a role by id.
+     *
+     * @param int $id
+     * @param array $relations
+     *
+     * @return Role|Model|null
+     */
+    public function findById(int $id, array $relations = []): Role|Model|null
+    {
+        return Role::with($relations)->firstWhere('id', $id);
+    }
+
+    /**
+     * Find a role by name.
+     *
+     * @param string $name
+     * @param string $guardName
+     *
+     * @return Role|null
+     */
+    public function findByName(string $name, string $guardName = 'api'): ?Role
+    {
+        return Role::findByName($name, $guardName);
+    }
+
+}
