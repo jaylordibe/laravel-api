436aa31120d10512570b39d70397de405b261fc6
jaylordibe <jaylordibe@gmail.com>
2025-02-06 18:42:26 +0800

    Updated valid route patterns

--- app/Constants/RoutePatternConstant.php
+++ app/Constants/RoutePatternConstant.php
@@ -6,6 +6,6 @@ class RoutePatternConstant extends BaseConstant
 {
 
     const string NUMERIC = '[0-9]+';
-    const string LETTERS = '[a-z]+';
+    const string LETTERS = '[A-Za-z0-9\-\_\.]+';
 
 }
