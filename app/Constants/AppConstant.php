92c87eb82541cbe1bb61499f6119d2b2ed732335
jaylordibe <jaylordibe@gmail.com>
2025-02-06 18:31:21 +0800

    Updated data sorting

--- app/Constants/AppConstant.php
+++ app/Constants/AppConstant.php
@@ -8,8 +8,8 @@ class AppConstant
     const int DEFAULT_PAGE = 1;
     const int DEFAULT_PER_PAGE = 10;
     const int MAX_PER_PAGE = 1000;
-    const string DEFAULT_DB_QUERY_SORT_FIELD = 'created_at';
-    const string DEFAULT_DB_QUERY_SORT_DIRECTION = 'asc';
+    const string DEFAULT_SORT_FIELD = 'created_at';
+    const string DEFAULT_SORT_DIRECTION = 'asc';
     const int DEFAULT_NUMBER_OF_RECORDS_PER_SHEET = 10000;
     const string DEFAULT_DATE_FORMAT = 'Y-m-d';
     const string DEFAULT_DATETIME_FORMAT = 'Y-m-d H:i:s';
