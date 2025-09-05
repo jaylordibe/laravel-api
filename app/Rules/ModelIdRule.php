a128da48fdcd128b8cfbe88a7651a2171239f942
jaylordibe <jaylordibe@gmail.com>
2025-09-05 10:22:16 +0800

    Rename ValidModelIdRule to ModelIdRule for consistency

--- app/Rules/ModelIdRule.php
+++ app/Rules/ModelIdRule.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Translation\PotentiallyTranslatedString;
 
-class ValidModelIdRule implements ValidationRule
+class ModelIdRule implements ValidationRule
 {
 
     protected string $model;
