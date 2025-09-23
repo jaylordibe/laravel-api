11a11c9c3de42d2ce7797af1d3155b7ec5bd8d6b
jaylordibe <jaylordibe@gmail.com>
2025-09-23 22:57:01 +0800

    Enhance middleware to prevent guest redirection and handle authentication exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Utils\ResponseUtil;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,8 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Prevent from redirecting to login page (which triggers RouteNotFoundException)
+        $middleware->redirectGuestsTo(function ($request) {
+            return null;
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            return ResponseUtil::unauthorized();
+        });
     })->create();
